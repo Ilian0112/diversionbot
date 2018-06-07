c9a58610595627320bbce41af5fec6fcaf016659
Ilian0112 <38587349+Ilian0112@users.noreply.github.com>
2018-06-07 14:04:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -24,7 +24,7 @@ function play(connection, message) {
 }
 
 bot.on("ready", function () {
-    bot.user.setActivity("DiversionBOT V2 - " + PREFIX + "help | Par Ilian, {
+    bot.user.setActivity("DiversionBOT V2 - " + PREFIX + "help | Par Ilian", {
         'type': 'STREAMING',
         'url': "https://www.twitch.tv/sebilein001"
 }),
